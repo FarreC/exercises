57e8e41e6c1dcd5e2178b2da53b2ea5a40828b25
Farre Cosmin <97farcos@skola.boras.se>
2019-09-19 14:05:09 +0200

    löser övning 2 på for-loopar

---
+++ exercise-for2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise_for2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Räknar från 40 till 30 baklänges
+            for(int i = 40; i > 29; i-- )
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
